e00b7fd03bda84d17b9a2f94cda28b4ab6f20c52
rajamri <45320184+rajamri@users.noreply.github.com>
2021-10-17 16:08:23 +0200

    add servomotor

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    servos.P1.setAngle(0)
+    basic.pause(500)
 })
+radio.onReceivedString(function (receivedString) {
+    servos.P1.setAngle(90)
+    basic.pause(500)
+})
+radio.setGroup(1)
